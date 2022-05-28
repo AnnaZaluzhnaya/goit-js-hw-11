4adc903d83f82491583a3045be6844e8a22ace71
AnnaZaluzhnaya <92920694+AnnaZaluzhnaya@users.noreply.github.com>
2022-05-29 00:48:25 +0200

    adding intermediate files

--- src/index.js
+++ src/index.js
@@ -1,1 +1,49 @@
 import './sass/main.scss';
+import NewsApiService from './js/new-api-service'
+import axios from "axios";
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import SimpleLightbox from 'simplelightbox';
+import "simplelightbox/dist/simple-lightbox.min.css";
+import photosTemplates from './templates/photos.hbs'
+
+const refs = {
+    form: document.querySelector('.search-form'),
+    input: document.querySelector('.search-input'),
+    searchBtn: document.querySelector('.search-button'),
+    loadBtn: document.querySelector('.load-more'),
+    gallery: document.querySelector('.gallery'),
+    
+};
+
+const newsApiService = new NewsApiService();
+
+refs.form.addEventListener('submit', onSearch);
+refs.loadBtn.addEventListener('click', onLoadMore);
+
+
+function onSearch(event) {
+    event.preventDefault();
+
+    clearPhoto();
+    newsApiService.nameQuery = event.currentTarget.elements.searchQuery.value;
+    newsApiService.resetPageNumber();
+    newsApiService.fetchPhoto().then(addMarkup);
+
+}
+
+function onLoadMore() {
+    newsApiService.fetchPhoto().then(addMarkup);
+}
+
+function addMarkup(photos) {
+    refs.gallery.insertAdjacentHTML('beforeend', photosTemplates(photos));
+}
+
+function clearPhoto() {
+    refs.gallery.innerHTML = "";
+}
+
+
+
+
+
---
+++ src/js/new-api-service.js
@@ -0,0 +1,35 @@
+export default class NewsApiService {
+    constructor() {
+        this.name = "";
+        this.pageNumber = 1;
+    }
+
+    fetchPhoto() {
+        const key = '27631880-b0639dc61f111cbc90b791bd4';
+        const url = `https://pixabay.com/api/?key=${key}&q=${this.name}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${this.pageNumber}`;
+
+        return fetch(url)
+            .then(response => response.json())
+            .then(data => {
+                this.incrementPageNumber();
+                
+                return data.hits;
+            });
+    }
+
+    incrementPageNumber() {
+        this.pageNumber += 1;
+    }
+
+    resetPageNumber() {
+        this.pageNumber = 1;
+    }
+
+    get nameQuery(){
+        return this.name;
+    }
+
+    set nameQuery(newName) {
+        this.name = newName;
+    }
+}
